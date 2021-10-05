6c9a91d727a900ca885cb31ccb5a8df642c72fb9
Jessica San <afraidspy@gmail.com>
2021-10-05 16:54:13 -0500

    Ejemplo sencillo de pruebas unitarias con jest library

---
+++ src/test/demo.test.js
@@ -0,0 +1,14 @@
+import '@testing-library/jest-dom';
+/**
+ * Archivo de pruebas
+ */
+describe('Pruebas en demo.test.js',()=>{
+    test('Entrando ..',()=>{
+
+        const cadena1 = "Jess";
+        const cadena2 = "Jess"
+    
+       expect(cadena1).toBe(cadena2);
+    
+    })
+})
